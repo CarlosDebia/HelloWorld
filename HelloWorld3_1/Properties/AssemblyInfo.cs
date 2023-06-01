e800487df998fe46bf4fc6f6178b45ded58649df
Carlos Debia <cdebia@gmail.com>
2023-06-01 17:32:00 -0300

    Agregar archivos de proyecto.

---
+++ HelloWorld3_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("HelloWorld3_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("HelloWorld3_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("2FD09D68-DA8D-45E6-9010-8BCBB392C968")]
+[assembly: AssemblyVersion("1.0.0.0")]
